bcba3dbcfb110ff65a5c203ec85dbbab900d1fc3
Onixfff <danilnikonov01@mail.ru>
2024-05-06 12:14:21 +0200

    Добавил установщик

--- DB/Database.cs
+++ DB/Database.cs
@@ -2,10 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
 
 namespace Diagram
 {
