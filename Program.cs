13d7e0ab67c04e2a0ef2e13ef267608e40b6ae6d
Onixfff <60624718+Onixfff@users.noreply.github.com>
2024-10-10 09:38:17 +0200

    Удалил пробел

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace Diagram
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
-
         }
     }
 }
