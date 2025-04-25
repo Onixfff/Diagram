fd4332c8bc3469bd85c9d584f918334f858286a8
Onixfff <danilnikonov01@mail.ru>
2025-04-25 11:46:33 +0200

    Создал модели для базы данных

---
+++ Models/DataPoints.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Diagram.Models.Data
+{
+    internal class DataPoints
+    {
+        public readonly int BatchNumber;
+        public readonly DateTime NowTime;
+        public readonly float Value;
+        public readonly int Time;
+
+        private DataPoints(int batchNumber, DateTime nowTime, float value, int time)
+        {
+            BatchNumber = batchNumber;
+            NowTime = nowTime;
+            Value = value;
+            Time = time;
+        }
+
+        public static DataPoints Create(int batchNumber, DateTime nowTime, float value, int time)
+        {
+            if (batchNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("Номер партии вышел за допустимый диапозон");
+            }
+
+            return new DataPoints(batchNumber, nowTime, value, time);
+        }
+    }
+}
---
+++ Models/Graph.cs
@@ -0,0 +1,20 @@
+﻿namespace Diagram.Models.Data
+{
+    internal class Graph
+    {
+        private readonly int Id;
+        private readonly string Comment;
+
+        private Graph(int id, string comment)
+        {
+            Id = id;
+            Comment = comment;
+        }
+
+        public static Graph Create(int id, string comment)
+        {
+            var graph = new Graph(id, comment);
+            return graph;
+        }
+    }
+}
