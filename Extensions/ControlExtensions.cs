888b4d6e317346b1071356da34b591a7b22410ef
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-06-13 11:29:17 +0200

    Сделал класс для проверка находиться ли элемент визуально в поле зрения пользователя

---
+++ Extensions/ControlExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Diagram.Extensions
+{
+    public class ControlExtensions
+    {
+        public static bool IsVisibleToUser(Control control)
+        {
+            if (control.Visible || control.IsDisposed) return false;
+
+            Rectangle screenBounds = control.RectangleToScreen(control.ClientRectangle);
+            Rectangle formBounds = control.FindForm()?.Bounds ?? Screen.PrimaryScreen.Bounds;
+
+            return screenBounds.IntersectsWith(formBounds);
+        }
+    }
+}
